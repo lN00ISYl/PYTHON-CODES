9edc3ce124d9fbc3a64ef9085fd88e3dbf2ae72c
lN00ISYl <karlossonrivo@gmail.com>
2023-09-28 19:05:45 +0800

    adjustments

--- LIST EXAMPLES.py
+++ LIST EXAMPLES.py
@@ -10,3 +10,47 @@ FIRST = "KARLOS"
 SECOND = "SON"
 
 print(FIRST[2])
+
+#check if the number you put in is in the list
+name = []
+name1 =int(input("Enter first number"))
+name2 =int(input("Enter second number"))
+name3 =int(input("Enter third number"))
+name4 =int(input("Enter fourth number"))
+name5 =int(input("Enter fifth number"))
+
+name.append(name1)
+name.append(name2)
+name.append(name3)
+name.append(name4)
+name.append(name5)
+print(name)
+cont = input("Enter s to continue:")
+if cont == "s":
+    num= int(input("enter a number"))
+    if num in name:
+        print("the number is in the list")    
+else:
+    print("nice")
+    
+
+#sort it form highest to lowest
+name = []
+name1 =int(input("Enter first number"))
+name2 =int(input("Enter second number"))
+name3 =int(input("Enter third number"))
+name4 =int(input("Enter fourth number"))
+name5 =int(input("Enter fifth number"))
+
+name.append(name1)
+name.append(name2)
+name.append(name3)
+name.append(name4)
+name.append(name5)
+
+
+name.sort(reverse=True)
+print(name)
+
+
+
--- LOOPS.py
+++ LOOPS.py
@@ -14,23 +14,23 @@
 
 #print(f"Hello {Name}")
         
-FREINDS = input("Enter your friends name: ")
+#FREINDS = input("Enter your friends name: ")
 
-while not FREINDS == "NONE":
- print(f"Your friends with {FREINDS}")
+#while not FREINDS == "NONE":
+ #print(f"Your friends with {FREINDS}")
 
-print("DONE")
+#print("DONE")
 
-#number1= int(input("Enter a number: "))
-#for number in range(number1):
- #   if number % 3 == 0 and number % 5 == 0:
-  #      print("Fizzbuzz")
-   #     continue
-#    elif number % 3  == 0 :
- #       print("fizz")
-#        continue
- #   elif number % 5 == 0:
-   #     print("buzz")
-  #      continue
-   # else:
-  #      print(number)
+number1= int(input("Enter a number: "))
+for number in range(number1):
+    if number % 3 == 0 and number % 5 == 0:
+        print("Fizzbuzz")
+        continue
+    elif number % 3  == 0 :
+        print("fizz")
+        continue
+    elif number % 5 == 0:
+        print("buzz")
+        continue
+    else:
+       print(number)
